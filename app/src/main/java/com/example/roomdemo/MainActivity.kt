6bd951f56023a770426a0def6401ef79eba48577
Danish <iamdanishk555@gmail.com>
2022-04-24 22:53:43 +0545

    Created Update/Delete Database Entries Functionality

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.roomdemo
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,6 +9,7 @@ import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomdemo.databinding.ActivityMainBinding
+import com.example.roomdemo.databinding.DialogUpdateBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -33,39 +36,115 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun addRecord(employeeDao: EmployeeDao){
+    fun addRecord(employeeDao: EmployeeDao) {
 
         val name = binding?.etName?.text.toString()
         val email = binding?.etEmailId?.text.toString()
 
-        if (name.isNotEmpty() && email.isNotEmpty()){
+        if (name.isNotEmpty() && email.isNotEmpty()) {
             lifecycleScope.launch {
                 employeeDao.insert(EmployeeEntity(name = name, email = email))
                 Toast.makeText(applicationContext, "Record Saved", Toast.LENGTH_LONG).show()
                 binding?.etName?.text?.clear()
                 binding?.etEmailId?.text?.clear()
             }
-        }
-        else{
-            Toast.makeText(applicationContext, "Name or Email cannot be Blanked!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Name or Email cannot be Blanked!",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
     private fun setupListOfDataIntoRecyclerView(
-        employeesList: ArrayList<EmployeeEntity>, employeeDao: EmployeeDao){
+        employeesList: ArrayList<EmployeeEntity>, employeeDao: EmployeeDao
+    ) {
 
-        if (employeesList.isNotEmpty()){
-            val itemAdapter = ItemAdapter(employeesList)
+        if (employeesList.isNotEmpty()) {
+            val itemAdapter = ItemAdapter(employeesList,
+                {
+                    updateId -> updateRecordDialog(updateId, employeeDao)
+                }) { deleteId ->
+                lifecycleScope.launch {
+                    employeeDao.fetchEmployeeById(deleteId).collect {
+                        if (it != null) {
+                            deleteRecordAlertDialog(deleteId, employeeDao, it)
+                        }
+                    }
+                }
+            }
 
             binding?.rvItemsList?.layoutManager = LinearLayoutManager(this)
             binding?.rvItemsList?.adapter = itemAdapter
             binding?.rvItemsList?.visibility = View.VISIBLE
             binding?.tvNoRecordsAvailable?.visibility = View.GONE
         }
-        else{
+        else {
             binding?.rvItemsList?.visibility = View.GONE
             binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
         }
+    }
 
+    private fun updateRecordDialog(id: Int, employeeDao: EmployeeDao) {
+        val updateDialog = Dialog(this, R.style.Theme_Dialog)
+        updateDialog.setCancelable(false)
+        val binding = DialogUpdateBinding.inflate(layoutInflater)
+        updateDialog.setContentView(binding.root)
+
+        lifecycleScope.launch {
+            employeeDao.fetchEmployeeById(id).collect {
+                if (it != null){
+                    binding.etUpdateName.setText(it.name)
+                    binding.etUpdateEmailId.setText(it.email)
+                }
+
+            }
+        }
+
+        binding.tvUpdate.setOnClickListener {
+            val name = binding.etUpdateName.text.toString()
+            val email = binding.etUpdateEmailId.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty()) {
+                lifecycleScope.launch {
+                    employeeDao.update(EmployeeEntity(id, name, email))
+                    Toast.makeText(applicationContext, "Record Updated", Toast.LENGTH_LONG).show()
+                    updateDialog.dismiss()
+                }
+            } else {
+                Toast.makeText(applicationContext, "Name or Email cannot be Blank!", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        binding.tvCancel.setOnClickListener {
+            updateDialog.dismiss()
+        }
+
+        updateDialog.show()
+    }
+
+    private fun deleteRecordAlertDialog(id: Int, employeeDao: EmployeeDao, employee: EmployeeEntity) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Delete Record")
+        builder.setMessage("Are you sure you wants to delete ${employee.name}.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        builder.setPositiveButton("Yes") { dialogInterface, _ ->
+            lifecycleScope.launch {
+                employeeDao.delete(EmployeeEntity(id))
+
+                Toast.makeText(applicationContext, "Record deleted successfully.", Toast.LENGTH_LONG).show()
+                dialogInterface.dismiss()
+            }
+        }
+
+        builder.setNegativeButton("No") { dialogInterface, _ ->
+            dialogInterface.dismiss()
+        }
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 }
