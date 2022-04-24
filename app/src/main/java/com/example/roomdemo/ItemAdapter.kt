810181ee0659550b76e61e15390f5483865ea75b
Danish <iamdanishk555@gmail.com>
2022-04-24 21:36:47 +0545

    Displayed the Database Entries on App UI Screen

--- app/src/main/java/com/example/roomdemo/ItemAdapter.kt
+++ app/src/main/java/com/example/roomdemo/ItemAdapter.kt
@@ -6,10 +6,11 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.roomdemo.databinding.ItemsRowBinding
 
-class ItemAdapter(private val items: ArrayList<EmployeeEntity>,
-                  private val updateListener: (id:Int) -> Unit,
-                  private val deleteListener: (id:Int) -> Unit) :
-    RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+class ItemAdapter(private val items: ArrayList<EmployeeEntity>)
+                  //private val updateListener: (id:Int) -> Unit,
+                  //private val deleteListener: (id:Int) -> Unit)
+
+    : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
     class ViewHolder(binding: ItemsRowBinding) : RecyclerView.ViewHolder(binding.root){
         val llMain = binding.llMain
@@ -40,11 +41,11 @@ class ItemAdapter(private val items: ArrayList<EmployeeEntity>,
         }
 
         holder.ivEdit.setOnClickListener {
-            updateListener.invoke(item.id)
+            //updateListener.invoke(item.id)
         }
 
         holder.ivDelete.setOnClickListener {
-            deleteListener.invoke(item.id)
+            //deleteListener.invoke(item.id)
         }
     }
 
--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.roomdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomdemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +23,13 @@ class MainActivity : AppCompatActivity() {
 
         binding?.btnAdd?.setOnClickListener {
             addRecord(employeeDao)
+        }
+
+        lifecycleScope.launch {
+            employeeDao.fetchAllEmployee().collect {
+                val list = ArrayList(it)
+                setupListOfDataIntoRecyclerView(list, employeeDao)
+            }
         }
     }
 
@@ -39,5 +49,23 @@ class MainActivity : AppCompatActivity() {
         else{
             Toast.makeText(applicationContext, "Name or Email cannot be Blanked!", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun setupListOfDataIntoRecyclerView(
+        employeesList: ArrayList<EmployeeEntity>, employeeDao: EmployeeDao){
+
+        if (employeesList.isNotEmpty()){
+            val itemAdapter = ItemAdapter(employeesList)
+
+            binding?.rvItemsList?.layoutManager = LinearLayoutManager(this)
+            binding?.rvItemsList?.adapter = itemAdapter
+            binding?.rvItemsList?.visibility = View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = View.GONE
+        }
+        else{
+            binding?.rvItemsList?.visibility = View.GONE
+            binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
+        }
+
     }
 }
